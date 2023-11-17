96193971ebc2af1a1b1e8e44b3c196abcadc8c21
fumimowdan <tecknuovo@zemis.co.uk>
2023-11-17 14:27:54 +0000

    Use Irp::Eligibility

--- config/initializers/irp_policy.rb
+++ config/initializers/irp_policy.rb
@@ -2,4 +2,6 @@ Irp.config_class = "PolicyConfiguration"
 Irp.claim_class = "Claim"
 Irp.task_class = "Task"
 Irp.school_class = "School"
-Irp.eligibility_class = "GenericEligibility"
+Irp.award_amount = 10_000
+
+Irp.admin_base_controller_class = "Admin::BaseAdminController"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_170530) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_17_115943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
@@ -170,10 +170,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_170530) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "generic_eligibilities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "irp_eligibilities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "school_id"
-    t.string "policy_name"
     t.integer "award_amount"
+    t.string "application_route"
+    t.boolean "one_year"
+    t.boolean "state_funded_secondary_school"
+    t.string "subject"
+    t.string "visa_type"
+    t.date "date_of_entry"
+    t.date "start_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
